e0efc30a09158c65715be9db18471884dc552efe
kurousa <r.takahashi.1026@gmail.com>
2025-03-09 22:03:13 +0900

    add: ヘルスチェック用の"/health"エンドポイントを追加 mod:anyhowを用いたエラーハンドリング

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn handler_health() -> StatusCode {
+    StatusCode::OK
 }
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/hello", get(hello_world));
+async fn main() -> Result<()> {
+    let app = Router::new()
+        .route("/health", get(handler_health))
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
     println!("Listening on {}", addr);
 
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
