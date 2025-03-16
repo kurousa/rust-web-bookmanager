7278cb59ff5a2b6a1f3d96ebd716a2f4faaacabb
kurousa <r.takahashi.1026@gmail.com>
2025-03-16 21:17:53 +0900

    add: adapterレイヤーへの具象実装

---
+++ adapter/src/database/model/book.rs
@@ -0,0 +1,31 @@
+use kernel::model::book::Book;
+use uuid::Uuid;
+
+/// book レコード型定義
+pub struct BookRow {
+    pub book_id: Uuid,
+    pub title: String,
+    pub author: String,
+    pub isbn: String,
+    pub description: String,
+}
+impl From<BookRow> for Book {
+    fn from(value: BookRow) -> Self {
+        // パターンマッチによる構造体からの値取り出し
+        let BookRow {
+            book_id,
+            title,
+            author,
+            isbn,
+            description,
+        } = value;
+        // kernelで定義したBook構造体の形式へ変換
+        Self {
+            id: book_id,
+            title,
+            author,
+            isbn,
+            description,
+        }
+    }
+}
