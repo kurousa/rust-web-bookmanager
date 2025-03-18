3f5b90c4ade53c9eac3b65358fd4cefff707cc88
kurousa <r.takahashi.1026@gmail.com>
2025-03-18 23:12:54 +0900

    feat: tracingを用いたロガーの初期化の追加 feat: tower_httpのTraceLayerを用いたHTTPリクエストのロギング実装

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,12 +1,46 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
-use anyhow::{Error, Result};
+use anyhow::{Context, Error, Result};
 use api::route::{book::build_book_routers, health::build_health_check_routers};
 use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
 use tokio::net::TcpListener;
+use tower_http::cors;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tower_http::LatencyUnit;
+use tracing::Level;
+
+use shared::env::{which, Environment};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+/// ロガーの初期化
+fn init_logger() -> Result<()> {
+    // ログレベルの設定
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    // ログのフォーマット設定
+    // ファイル名、行番号、ターゲット名を表示
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(true);
+
+    // ロガーの登録
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -25,15 +59,34 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routers())
         .merge(build_book_routers())
+        // TODO: implement
+        //.merge(vi::routes())
+        //.merge(auth::routes())
+        .layer(cors())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO))
+                .latency_unit(LatencyUnit::Millis),
+        )
         .with_state(registry);
 
     // サーバーの起動
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await?;
-
-    println!("Listening on {}", addr);
-
-    axum::serve(listener, app).await.map_err(Error::from)
+    tracing::info!("Listening on: {}", addr);
+    axum::serve(listener, app)
+        .await
+        // 起動失敗時のログを tracing::error! で出力する
+        .context("Unexpected error in server")
+        .inspect_err(|e| {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "Unexpected error",
+            )
+        })
 }
 
 // #[tokio::test]
