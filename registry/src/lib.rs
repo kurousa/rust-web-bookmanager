a8da84f11069f294a9d924f27db9a00399b93ff7
kurousa <r.takahashi.1026@gmail.com>
2025-03-22 11:22:02 +0900

    add: registryレイヤーへのuser_repository追加

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -3,12 +3,14 @@ use std::sync::Arc;
 use adapter::{
     database::ConnectionPool,
     redis::RedisClient,
-    repository::{auth::AuthRepositoryImpl, health::HealthCheckRepositoryImpl},
+    repository::{
+        auth::AuthRepositoryImpl, health::HealthCheckRepositoryImpl, user::UserRepositoryImpl,
+    },
 };
 
 use adapter::repository::book::BookRepositoryImpl;
 use kernel::repository::{
-    auth::AuthRepository, book::BookRepository, health::HealthCheckRepository,
+    auth::AuthRepository, book::BookRepository, health::HealthCheckRepository, user::UserRepository,
 };
 
 use shared::config::AppConfig;
@@ -18,6 +20,7 @@ pub struct AppRegistry {
     health_check_repository: Arc<dyn HealthCheckRepository>,
     book_repository: Arc<dyn BookRepository>,
     auth_repository: Arc<dyn AuthRepository>,
+    user_repository: Arc<dyn UserRepository>,
 }
 
 impl AppRegistry {
@@ -33,11 +36,13 @@ impl AppRegistry {
             redis_client.clone(),
             app_config.auth.ttl,
         ));
+        let user_repository = Arc::new(UserRepositoryImpl::new(pool.clone()));
 
         Self {
             health_check_repository,
             book_repository,
             auth_repository,
+            user_repository,
         }
     }
 
@@ -51,5 +56,9 @@ impl AppRegistry {
 
     pub fn auth_repository(&self) -> Arc<dyn AuthRepository> {
         self.auth_repository.clone()
+    }
+
+    pub fn user_repository(&self) -> Arc<dyn UserRepository> {
+        self.user_repository.clone()
     }
 }
