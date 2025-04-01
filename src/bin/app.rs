47fad5120f4911f780d8144cabdd84e357e3ea93
kurousa <r.takahashi.1026@gmail.com>
2025-04-02 00:10:54 +0900

    add: OpenTelemetryによるJaegerでのトレース可視化

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -7,6 +7,7 @@ use adapter::{database::connect_database_with, redis::RedisClient};
 use anyhow::{Context, Result};
 use api::route::{auth, v1};
 use axum::{http::Method, Router};
+use opentelemetry::global;
 use registry::AppRegistryImpl;
 use shared::config::AppConfig;
 use tokio::net::TcpListener;
@@ -27,6 +28,25 @@ fn init_logger() -> Result<()> {
         Environment::Development => "debug",
         Environment::Production => "info",
     };
+
+    // OpenTelemetryとの接続定義
+    let jaeger_host = std::env::var("JAEGER_HOST")?;
+    let jaeger_port = std::env::var("JAEGER_PORT")?;
+    dbg!(&jaeger_host, &jaeger_port);
+    let jaeger_max_packet_size =
+        std::env::var("JAEGER_MAX_PACKET_SIZE").unwrap_or("8192".to_string());
+    let jaeger_endpoint = format!("{}:{}", jaeger_host, jaeger_port);
+    dbg!(&jaeger_endpoint);
+
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    let jaeger_tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_endpoint(jaeger_endpoint)
+        .with_service_name("rust-web-bookmanager")
+        .with_auto_split_batch(true)
+        .with_max_packet_size(jaeger_max_packet_size.parse::<usize>()?)
+        .install_simple()?;
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(jaeger_tracer);
+
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
 
     // ログのフォーマット設定
@@ -43,6 +63,7 @@ fn init_logger() -> Result<()> {
     tracing_subscriber::registry()
         .with(subscriber)
         .with(env_filter)
+        .with(opentelemetry)
         .try_init()?;
 
     Ok(())
@@ -93,6 +114,8 @@ async fn bootstrap() -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     tracing::info!("Listening on: {}", addr);
     axum::serve(listener, app)
+        // グレースフルシャットダウン時に実行する処理
+        .with_graceful_shutdown(shutdown_signal())
         .await
         // 起動失敗時のログを tracing::error! で出力する
         .context("Unexpected error in server")
@@ -103,4 +126,39 @@ async fn bootstrap() -> Result<()> {
                 "Unexpected error",
             )
         })
+}
+
+async fn shutdown_signal() {
+    fn purge_spans() {
+        global::shutdown_tracer_provider();
+    }
+
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install SIGTERM signal handler")
+            .recv()
+            .await
+            .expect("Failed to receive SIGTERM signal");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            tracing::info!("Ctrl-Cを受信しました。");
+            purge_spans()
+        },
+        _ = terminate => {
+            tracing::info!("SIGTERMを受信しました。");
+            purge_spans()
+        }
+    }
 }
